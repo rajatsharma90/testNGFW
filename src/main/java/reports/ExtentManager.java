c2dc53d2336a0abefdb0e6c3168abd95267e3e06
Rajat Sharma <rajatsharma.9001@gmail.com>
2020-08-05 17:22:45 -0400

    Created ExtentManger Utility and changes in pom.xml

---
+++ src/main/java/reports/ExtentManager.java
@@ -0,0 +1,31 @@
+package reports;
+
+import java.io.File;
+import java.util.Date;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentManager {
+	static ExtentReports report;
+	Date d=new Date();
+	String folderName=d.toString().replace(" ", "_").replace(":", "_");
+	
+	public ExtentReports getReport() {
+		if(report==null) {
+			report=new ExtentReports();
+			String reportFolderpath=System.getProperty("user.dir")+"/report/"+folderName;
+			String screenShotfolderPath=reportFolderpath+"/Screenshots/";
+			File f= new File(screenShotfolderPath);
+			f.mkdirs();
+			ExtentSparkReporter reporter=new ExtentSparkReporter(reportFolderpath);
+			reporter.config().setDocumentTitle("Automation report");
+			reporter.config().setReportName("Test Reports");
+			reporter.config().setTheme(Theme.DARK);
+			report.attachReporter(reporter);
+		}
+		return report;
+
+	}
+}
